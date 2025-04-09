c0f665a5727c3b145c1711b90b5ff65abfadb593
FelipeFMA <felipefmavelar@gmail.com>
2025-04-09 15:20:58 -0300

    removed language selection from the settings tab, removed a bunch of unused files and added vscode settings to gitignore

--- src/core/SettingsTab.cpp
+++ src/core/SettingsTab.cpp
@@ -18,16 +18,6 @@ SettingsTab::SettingsTab()
     pack_start(autostart_check_, Gtk::PACK_SHRINK);
     pack_start(notifications_check_, Gtk::PACK_SHRINK);
 
-    auto lang_label = Gtk::make_managed<Gtk::Label>("Language:");
-    lang_label->set_halign(Gtk::ALIGN_START);
-    pack_start(*lang_label, Gtk::PACK_SHRINK);
-
-    language_combo_.append("en");
-    language_combo_.append("pt");
-    language_combo_.append("es");
-    language_combo_.set_active_text("en");
-    pack_start(language_combo_, Gtk::PACK_SHRINK);
-
     load_settings();
 
     show_all_children();
@@ -48,15 +38,11 @@ void SettingsTab::load_settings() {
 
     autostart_check_.set_active(settings_["autostart"] == "1");
     notifications_check_.set_active(settings_["notifications"] == "1");
-    if (!settings_["language"].empty()) {
-        language_combo_.set_active_text(settings_["language"]);
-    }
 }
 
 void SettingsTab::save_settings() {
     settings_["autostart"] = autostart_check_.get_active() ? "1" : "0";
     settings_["notifications"] = notifications_check_.get_active() ? "1" : "0";
-    settings_["language"] = language_combo_.get_active_text();
 
     std::ofstream outfile("/home/felipe/.config/ultimate-control/settings.conf");
     if (!outfile.is_open()) {
